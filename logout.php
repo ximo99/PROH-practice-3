4d7bdf28ecb0c6cf83e1f98407f304aa547c89d2
ximo99 <125189091+ximo99@users.noreply.github.com>
2023-10-10 09:52:11 +0200

    logout finished

---
+++ logout.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+
+// Eliminar todas las variables de sesión
+$_SESSION = array();
+
+// Destruir la sesión
+session_destroy();
+
+// Redirigir al usuario a la página de inicio de sesión
+header("Location: index.php");
+exit();
+?>
